5eaad9090aee1983c345cfb9e4d42bcc22ca84be
mspolermo <mspolermo@gmail.com>
2023-08-02 14:42:33 +0500

    allImages Compressed

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-
 import './i18n';
 
 const root = ReactDOM.createRoot(
